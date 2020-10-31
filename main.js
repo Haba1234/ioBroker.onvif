21a7d92b11317f82e2183bb86b7b3189992c30f7
JustMe0815 <dennis.bradenton@gmail.com>
2020-10-31 21:59:52 +0100

    Update main.js

--- main.js
+++ main.js
@@ -340,11 +340,12 @@ function PullPointSubscription(cam, id, callback){
 			}
 		}
 	}
+);
 					cam.renew({timeout: 5000, messageLimit: 10}, (err, events) => {
 						if (err) {
 						adapter.log.warn(`startCameras (${id}) pullMessages: ERROR - renew subscription failed. Resubscribing to events`);
 						}
-					}););
+					});
 }
 
 async function startCameras(){
